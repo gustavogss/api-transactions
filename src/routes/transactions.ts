88fc40838cd215ca5296bdd826c01b0189677cfa
gustavogss <gustavogss.dev@gmail.com>
2025-03-14 01:12:52 -0300

    feat: created route get transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,23 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParmsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParmsSchema.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+    return { transaction }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
